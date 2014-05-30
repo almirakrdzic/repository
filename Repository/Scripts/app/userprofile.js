5abb3c0ec1bb12cdc647b928f23b7ed47f65af90
almirakrdzic <a.lmirakrdic@gmail.com>
2014-05-30 14:38:56 +0200

    dorade

--- Repository/Scripts/app/userprofile.js
+++ Repository/Scripts/app/userprofile.js
@@ -1,5 +1,5 @@
 ﻿angular.module('main')
-    .controller('UserProfileController', ['$scope', 'userRepository', '$routeParams', function ($scope, userRepository, $routeParams) {
+    .controller('UserProfileController', ['$scope', 'userRepository', '$routeParams', 'bookRepository', function ($scope, userRepository, $routeParams, bookRepository) {
 
         function getUser() {
             userRepository.getUser($routeParams.username, function (results) {
@@ -8,5 +8,34 @@
             })
         };
         getUser();
+
+        $scope.books = [];
+        $scope.booksSize = 0;
+        $scope.getUploads = function() {
+            bookRepository.getUploads($routeParams.username, function (results) {
+                data = results;
+                data1 = data;
+                $scope.books = data1.slice(0, $scope.itemsPerPage);
+                $scope.booksSize = results.length;
+            })
+        };
+        $scope.getDownloads = function() {
+            bookRepository.getDownloads($routeParams.username, function (results) {
+                data = results;
+                data1 = data;
+                $scope.books = data1.slice(0, $scope.itemsPerPage);
+                $scope.booksSize = results.length;
+            })
+        };
+
+        $scope.maxSize = 5;
+        $scope.currentPage = 1;
+        $scope.itemsPerPage = 4;
+        $scope.$watch("currentPage", function (newValue, oldValue) {
+            data1 = data;
+            offset = (newValue - 1) * $scope.itemsPerPage;
+            limit = $scope.itemsPerPage;
+            $scope.books = data1.slice(offset, offset + limit);
+        });
 
     }]);
