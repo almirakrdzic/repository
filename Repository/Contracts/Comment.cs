2585bf72c785a3bcb12f1d2fcba192c8f1ac8566
almirakrdzic <a.lmirakrdic@gmail.com>
2014-07-04 19:49:13 +0200

    Eyecandy

--- Repository/Contracts/Comment.cs
+++ Repository/Contracts/Comment.cs
@@ -19,6 +19,7 @@ namespace Repository.Controllers
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         public string Text { get; set; }       
          
+        [DataMember]
         public User IdUser { get; set; }        
        
          [DataMember(IsRequired=true)]       
